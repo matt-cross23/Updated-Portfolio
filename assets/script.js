2096886e04ab38c675ea2c0107e7f48151ef1790
Matthew Cross <matthewcross1017@gmail.com>
2021-10-29 16:45:36 -0400

    work yesterday

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,9 @@
-console.log("Im connected")
+console.log("Im connected");
+var descriptionP = document.getElementById('descript1');
+let button1 = document.getElementsByClassName('buttonP');
+
+function showText(){
+    descriptionP.style.display == "block";
+};
+
+    button1.addEventListener("click", showText);
