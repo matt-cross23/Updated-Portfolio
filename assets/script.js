807169ee7c6f61d78bf543d37e2f702879684d2c
Matthew Cross <88978036+matt-cross23@users.noreply.github.com>
2021-10-29 15:48:18 -0400

    Update Script

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,14 @@
-console.log("Im connected")
+const displayP = document.querySelector('#btnText')
+let p = document.getElementById('clickText');
+
+
+function toggleText() {
+  if (p.style.display === "block") {
+    p.style.display = "none";
+    displayP.textContent = "Click for details";
+  } else {
+    p.style.display = "block";
+
+    displayP.textContent = "Click to minimize";
+  }
+}
